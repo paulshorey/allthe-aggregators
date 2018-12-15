5b084c4cee1183a6f7cf1af1585c6b6680fcf28b
Paul Shorey <pshorey@beyond.ai>
2018-12-15 14:36:05 -0800

    node _remify.js

--- public/static/css/_remify.js
+++ public/static/css/_remify.js
@@ -1,5 +1,12 @@
-// ([(|,|-| |:]*?)([0-9]*?)px
-// $1($2/16)rem
+/*
+	This Node.js script reads and alters a list of files
+	by searching `([(|,|-| |:]*?)([0-9]*?)px` and replacing `${first}${(second/16)}rem`
+	*
+	This is useful for converting static css styles using "px"
+	for a responsive web app, using the dynamic "rem" unit
+	*
+	A good conversion ratio is usually 16px = 1rem (ex: 10px = 0.625rem)
+*/
 
 var fs = require('fs');
 
@@ -24,4 +31,7 @@ function readWriteAsync(fileName) {
   });
 
 }
+
+// process files
+readWriteAsync("./blueprint-rem.css");
 readWriteAsync("./blueprint-datetime-rem.css");
